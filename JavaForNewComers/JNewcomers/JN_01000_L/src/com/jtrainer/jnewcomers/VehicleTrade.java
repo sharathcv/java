c014bb4876fedbcd148eb740ea836b48b1590597
sharathcv <sharathcv@gmail.com>
2025-05-25 16:10:31 +1000

    Basic Control flow changes

--- JavaForNewComers/JNewcomers/JN_01000_L/src/com/jtrainer/jnewcomers/VehicleTrade.java
+++ JavaForNewComers/JNewcomers/JN_01000_L/src/com/jtrainer/jnewcomers/VehicleTrade.java
@@ -9,34 +9,61 @@ public class VehicleTrade {
 
     private void run() {
         System.out.println("Welcome to the vehicle trade!");
-        System.out.println("What do you want to do?");
-        System.out.println("1) List vehicle categories");
-        System.out.println("2) Search used vehicle");
-        System.out.println("3) Credit advice");
+        int selection;
 
-        Scanner scanner = new Scanner(System.in);
-        int selection = scanner.nextInt();
+        do {
+            System.out.println();
+            System.out.println("What do you want to do?");
+            System.out.println("1) List vehicle categories");
+            System.out.println("2) Search used vehicle");
+            System.out.println("3) Credit advice");
+            System.out.println(("9) Exit"));
 
-        if (selection == 1) {
-            System.out.println("We trade in the following vehicle categories:");
-            System.out.println("Limousine");
-            System.out.println("EstateCar");
-            System.out.println("Cabriolet");
-            System.out.println("SportsCar");
-        }
+            Scanner scanner = new Scanner(System.in);
+            selection = scanner.nextInt();
 
-        if (selection == 2) {
-            System.out.println("At the moment we have no used vehicles on offer.");
-        }
+            if (selection == 9) { break; }
 
-        if (selection == 3) {
-            System.out.println("How expensive is the vehicle you want to finance?");
-            int vehiclePrice = scanner.nextInt();
-            System.out.println("How many months would you like to finance the vehicle for?");
-            int numberOfMonthlyPayments = scanner.nextInt();
-            double monthlyPayment = (vehiclePrice * 1.03 + 20.0) / numberOfMonthlyPayments - 1;
-            System.out.println("Monthly payment");
-            System.out.println(monthlyPayment);
-        } 
+            if (selection == 1) {
+                System.out.println("We trade in the following vehicle categories:");
+                System.out.println("Limousine");
+                System.out.println("EstateCar");
+                System.out.println("Cabriolet");
+                System.out.println("SportsCar");
+            }
+
+            if (selection == 2) {
+                System.out.println("Which vehicle category are you looking for?");
+                String vehicleCategory = scanner.next();
+                if (vehicleCategory.equalsIgnoreCase("SportsCar")
+                    || vehicleCategory.equalsIgnoreCase("Cabriolet")) {
+                    System.out.println("At the moment we have no used sports cars on offer.");
+                } else if (vehicleCategory.equalsIgnoreCase("Limousine")
+                    || vehicleCategory.equalsIgnoreCase("EstateCar")) {
+                    System.out.println("All right, we are looking for used vehicles of the desired category "
+                            + vehicleCategory + ".");
+                } else {
+                    System.out.println("Unknown vehicle category: " + vehicleCategory);
+                }
+            }
+
+            if (selection == 3) {
+                System.out.println("How expensive is the vehicle you want to finance?");
+                int vehiclePrice = scanner.nextInt();
+                System.out.println("How many months would you like to finance the vehicle for?");
+                int numberOfMonthlyPayments = scanner.nextInt();
+
+                if (numberOfMonthlyPayments > 0
+                    && numberOfMonthlyPayments % 12 == 0) {
+                    double monthlyPayment = (vehiclePrice * 1.03 + 20.0) / numberOfMonthlyPayments - 1;
+                    System.out.println("Please pay " + numberOfMonthlyPayments
+                            + " monthly payments of $" + monthlyPayment);
+                } else {
+                    System.out.println("Sorry, we cannot offer this financing.");
+                }
+            }
+        } while(selection != 9);
+
+        System.out.println("Bye and visit us again soon.");
     }
 }
