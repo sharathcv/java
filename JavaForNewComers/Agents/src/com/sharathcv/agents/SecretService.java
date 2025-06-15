55be0d931015a1b7bad95e377f613a9d79dac0f9
sharathcv <sharathcv@gmail.com>
2025-06-15 12:48:11 +1000

    Agent data using arrays

--- JavaForNewComers/Agents/src/com/sharathcv/agents/SecretService.java
+++ JavaForNewComers/Agents/src/com/sharathcv/agents/SecretService.java
@@ -3,27 +3,16 @@ package com.sharathcv.agents;
 import java.util.Scanner;
 
 public class SecretService {
-
     public static void main(String[] args) {
         new SecretService().run();
     }
 
     private void run() {
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("How many agents are in your team?");
-        int agentCount = scanner.nextInt();
-
-        Agent[] agents = new Agent[agentCount];
-
-        for (int i = 0; i < agentCount; i++) {
-            System.out.print("Please enter the first name of agent " + (i + 1) + ": ");
-            String firstName = scanner.next();
-            System.out.print("Please enter the last name of agent " + (i + 1) + ": ");
-            String lastName = scanner.next();
-            Agent agent = new Agent(firstName, lastName);
-            agents[i] = agent;
-        }
+        Agent[] agents = new Agent[] {
+                new Agent("Top", "Agent"),
+                new Agent("Super", "Agent"),
+                new Agent("Secret", "Agent")
+        };
 
         introduceLikeSecretAgents(agents);
     }
