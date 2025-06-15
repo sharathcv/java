8e1b93ef1d7c810a15cccf68536320529fcad13d
sharathcv <sharathcv@gmail.com>
2025-06-15 12:28:16 +1000

    Agent class updated

--- JavaForNewComers/Agents/src/com/sharathcv/agents/Agent.java
+++ JavaForNewComers/Agents/src/com/sharathcv/agents/Agent.java
@@ -1,5 +1,31 @@
 package com.sharathcv.agents;
 
 public class Agent {
+    private String firstName;
+    private String lastName;
 
+    public Agent(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    private void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    private void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String toString() {
+        return firstName + " " + lastName;
+    }
 }
