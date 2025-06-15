7db94d5692a72e1285fbf1f76bfee2fed18e14bf
sharathcv <sharathcv@gmail.com>
2025-06-15 12:37:15 +1000

    Modified to for loop

--- JavaForNewComers/Agents/src/com/sharathcv/agents/SecretService.java
+++ JavaForNewComers/Agents/src/com/sharathcv/agents/SecretService.java
@@ -3,6 +3,7 @@ package com.sharathcv.agents;
 import java.util.Scanner;
 
 public class SecretService {
+
     public static void main(String[] args) {
         new SecretService().run();
     }
@@ -15,26 +16,22 @@ public class SecretService {
 
         Agent[] agents = new Agent[agentCount];
 
-        int i = 0;
-        while (i < agentCount) {
+        for (int i = 0; i < agentCount; i++) {
             System.out.print("Please enter the first name of agent " + (i + 1) + ": ");
             String firstName = scanner.next();
             System.out.print("Please enter the last name of agent " + (i + 1) + ": ");
             String lastName = scanner.next();
             Agent agent = new Agent(firstName, lastName);
             agents[i] = agent;
-            i++;
         }
 
         introduceLikeSecretAgents(agents);
     }
 
     private void introduceLikeSecretAgents(Agent[] agents) {
-        int i = 0;
-        while (i < agents.length) {
-            System.out.println("My name is " + agents[i].getLastName() + ", " + agents[i].getFirstName() + " "
-                    + agents[i].getLastName());
-            i++;
+        for (Agent agent : agents) {
+            System.out.println(
+                    "My name is " + agent.getLastName() + ", " + agent.getFirstName() + " " + agent.getLastName());
         }
     }
 }
